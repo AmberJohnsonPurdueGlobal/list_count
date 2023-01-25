7b27383f8678778e54b94919e5addaa6e05cd6bc
AmberJohnsonPurdueGlobal <107158761+AmberJohnsonPurdueGlobal@users.noreply.github.com>
2023-01-25 10:37:57 -0800

    Create sorted_function.py

---
+++ sorted_function.py
@@ -0,0 +1,8 @@
+#Populate a list called "games"
+games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
+#Sort the list "games" in ascending order creating a new list called "games_sorted"
+games_sorted = sorted(games)
+#Display the "games" list
+print(games)
+#Display the "games_sorted" list
+print(games_sorted)
