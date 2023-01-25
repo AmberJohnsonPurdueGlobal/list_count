d0f81850f9b6e3b55e0e9e379ff739765d5c972c
AmberJohnsonPurdueGlobal <107158761+AmberJohnsonPurdueGlobal@users.noreply.github.com>
2023-01-25 10:07:24 -0800

    Create votes.py

---
+++ votes.py
@@ -0,0 +1,6 @@
+#create a "votes" list containing each vote results
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+#Create a new list "jake_votes" containig the number of votes won by Jake
+jake_votes = votes.count("Jake")
+#Display the number of votes in "jake_votes" list
+print(jake_votes)
