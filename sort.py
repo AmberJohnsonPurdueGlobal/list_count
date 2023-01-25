fef237aceca605053569ba7724d4099e615cb5b4
AmberJohnsonPurdueGlobal <107158761+AmberJohnsonPurdueGlobal@users.noreply.github.com>
2023-01-25 10:21:42 -0800

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,16 @@
+#populate a list of addresses called "address"
+addresses = ["221 B Baker St.", "42 Wallaby Way", "12 Grimmauld Place", "742 Evergreen Terrace", "1600 Pennsylvania Ave", "10 Downing St."]
+#Sort the "address" list in ascending order
+addresses.sort()
+#Display the "address" list
+print(addresses)
+#populate a new list of names called "names"
+names = ["Ron", "Hermione", "Harry", "Albus", "Sirius"]
+#sort the "name" list in ascending order
+names.sort()
+#populate a new list of cities called "cities"
+cities = ["London", "Paris", "Rome", "Los Angeles", "New York"]
+#Sort the "cities" list in descending order
+cities.sort(reverse=True)
+#Display the "cities" list
+print(cities)
